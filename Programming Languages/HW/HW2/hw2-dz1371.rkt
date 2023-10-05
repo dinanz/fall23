6e5e1775bfafe97c6aa53b3e39fa40451a1ba04a
dinanz <dz1371@nyu.edu>
2023-10-05 11:24:10 -0400

    update

--- Programming Languages/HW/HW2/hw2-dz1371.rkt
+++ Programming Languages/HW/HW2/hw2-dz1371.rkt
@@ -1,5 +1,5 @@
 #|
-Question 1
+Question 1 DONE
 |#
 (define (chain funcs v)
   (if
@@ -9,7 +9,7 @@ Question 1
 
 
 #|
-Question 2
+Question 2 DONE
 |#
 (define (chain_odd funcs v)
   (if (or
@@ -39,6 +39,7 @@ Question 3
       ))
 
 (define (zip l1 l2) (zip_helper l1 l2 '()))
+(zip '(1 2 3) '(4 5 6))
 
 #|
 zip (1 2 3) (4 5 6) ()
@@ -58,10 +59,60 @@ Question 4
 |#
 (define (unzip_help zipped unzipped)
   (if (null? zipped) unzipped      ; if null return unzipped list
-      (unzip_help (cdr zipped) (list unzipped (cons (car (car zipped)) (cdr (car zipped))))
+      (unzip_help (cdr zipped)
+                  (list
+                     (list (car unzipped)  (car (car zipped)))
+                     (list (car (cdr unzipped))  (car (cdr (car zipped)))))
              )))
-(define (unzip zipped) (unzip_help zipped '()))             
+(define (unzip zipped) (unzip_help zipped '(() ()) ) )             
+
+(unzip '((1 4) (2 5) (3 6)))
 
 
+
+
+
+
+
+#|
+Question 6 DONE
+|#
+(define (reverse_help x rX)
+  (if (null? x) rX ; return full reversed list
+      (reverse_help (cdr x) (cons (car x) rX)
+               )))
+
+(define (reverseX x) (reverse_help x '()))
+      
+
+
+#|
+Question 7
+|#
+(define (interleave_help l1 l2 res)
+  (if (or (null? l1) (null? l2)) res
+      (interleave_help (cdr l1) (cdr l2)
+                       (cons res (cons (car l1) (cons (car l2) '())))
+)))
+; USE LET
+
+   
+(define (interleave_outer l1 l2) (interleave_help l1 (reverse l2) '()))
+
+(interleave_outer '(1 2 3) '(a b c))
+
+
+
+#|
+Question 8 DONE
+|#
+(define (count_help lst x count)
+  (cond
+    ((null? lst) count) ; return full reversed list
+    ((equal? (car lst) x)  (count_help (cdr lst) x (+ 1 count)))
+    (else  (count_help (cdr lst) x count))
+               ))
+
+(define (count-occurrences lst x) (count_help lst x 0))
 
 
